27a6779da9f1b6a6f0dc73988c5c386616a6bad3
jighyanshu kumar <jighyanshu@jighyanshus-MacBook-Air.local>
2025-08-16 11:44:18 +0530

    git host

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/sweet-memory-site/", // 👈 IMPORTANT: set this to your repo name
 }));
