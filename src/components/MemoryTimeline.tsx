8555fe089d199ec8fa125fbf88ed73c8085cf69a
jighyanshu kumar <jighyanshu@jighyanshus-MacBook-Air.local>
2025-08-16 12:55:40 +0530

    fixedmedia6

--- src/components/MemoryTimeline.tsx
+++ src/components/MemoryTimeline.tsx
@@ -174,7 +174,6 @@ const MemoryTimeline = () => {
                         src={memory.media}
                         autoPlay
                         loop
-                        muted
                         playsInline
                         className="w-full max-w-2xl h-80 md:h-96 object-cover object-center rounded-2xl shadow-soft mx-auto mb-4"
                       />
